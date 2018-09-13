b5511594f3084da93ca2255918269376d554cca1
Jatin Sanghvi <jatin085@yahoo.com>
2018-09-13 04:46:23 -0700

    Follow Demo: View-First MVVM Hookup

---
+++ MVVMHookupDemo/MVVMHookupDemo/Customers/CustomerListView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace MVVMHookupDemo.Customers
+{
+    /// <summary>
+    /// Interaction logic for CustomerListView.xaml
+    /// </summary>
+    public partial class CustomerListView : UserControl
+    {
+        public CustomerListView()
+        {
+            InitializeComponent();
+            this.DataContext = new CustomerListViewModel();
+        }
+    }
+}
---
+++ MVVMHookupDemo/MVVMHookupDemo/Customers/CustomerListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MVVMHookupDemo.Services;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using Zza.Data;
+
+namespace MVVMHookupDemo.Customers
+{
+    internal sealed class CustomerListViewModel
+    {
+        public ObservableCollection<Customer> Customers { get; set; }
+
+        private ICustomersRepository _repo = new CustomersRepository();
+
+        public CustomerListViewModel()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+            {
+                return;
+            }
+
+            Customers = new ObservableCollection<Customer>(_repo.GetCustomersAsync().Result);
+        }
+    }
+}
