3a4b0b6d6e4ec3b503e945041457061a790a6ee0
Jatin Sanghvi <jatin085@yahoo.com>
2018-09-17 21:30:03 -0700

    Follow Demo: Building Out an MVVM Hierarchy

---
+++ ZzaDesktop/ZzaDesktop/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZzaDesktop
+{
+    internal sealed class MainWindowViewModel
+    {
+        private readonly CustomerListViewModel customerViewModel = new CustomerListViewModel();
+        private readonly OrderPrepViewModel orderPrepViewModel = new OrderPrepViewModel();
+        private readonly OrderViewModel orderViewModel = new OrderViewModel();
+
+        public object CurrentViewModel { get; set; }
+    }
+}
