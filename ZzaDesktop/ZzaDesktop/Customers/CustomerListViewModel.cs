b139e86e9273cb8b7864b2d3320fad9e53a04399
Jatin Sanghvi <jatin085@yahoo.com>
2018-09-18 22:32:15 -0700

    Follow Demo: Data Loading into a ViewModel

--- ZzaDesktop/ZzaDesktop/Customers/CustomerListViewModel.cs
+++ ZzaDesktop/ZzaDesktop/Customers/CustomerListViewModel.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
+using Zza.Data;
 
 namespace ZzaDesktop
 {
     internal sealed class CustomerListViewModel : BindableBase
     {
+        private readonly ICustomersRepository customersRepository = new CustomersRepository();
+
+        private ObservableCollection<Customer> customers;
+
+        public CustomerListViewModel()
+        {
+             this.PlaceOrderCommand = new RelayCommand<Customer>(this.OnPlaceOrder);
+        }
+
+        public ObservableCollection<Customer> Customers
+        {
+            get => this.customers;
+            set => this.SetProperty(ref this.customers, value);
+        }
+
+        public RelayCommand<Customer> PlaceOrderCommand { get; private set; }
+
+        public async void LoadCustomers()
+        {
+            this.Customers = new ObservableCollection<Customer>(await this.customersRepository.GetCustomersAsync());
+        }
+
+        private void OnPlaceOrder(Customer customer)
+        {
+        }
     }
 }
